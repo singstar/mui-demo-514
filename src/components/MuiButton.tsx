2a29362302925d0a34c20cd05c659ddb7a42610c
YG <eimageyang@gmail.com>
2023-08-28 14:36:34 +0800

    add buttons

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -2,10 +2,19 @@ import { Stack, Button } from "@mui/material"
 
 export const MuiButton = () => {
   return (
-    <Stack spacing={2} direction='row'>
-        <Button variant='text' href="https://google.com"> Text 1</Button>
-        <Button variant='contained'>Text 1</Button>
-        <Button variant='outlined'>Text 1</Button>
+    <Stack spacing={4}>
+      <Stack spacing={2} direction='row'>
+          <Button variant='text' href="https://google.com"> Text 1</Button>
+          <Button variant='contained'>Text 1</Button>
+          <Button variant='outlined'>Text 1</Button>
+      </Stack>
+      <Stack spacing={5} direction='row'>
+          <Button variant='contained' color='primary'>PRIMARY</Button>
+          <Button variant='contained' color='secondary'>SECONDARY</Button>
+          <Button variant='outlined' color='info'>Infor</Button>
+          <Button variant='contained' color='error'>ERROR</Button>
+      </Stack >
     </Stack>
+   
   )
 }
